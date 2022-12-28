51e745c9da7f266e10cd1fe30fb2d50631eb9d5d
loveofc <skyhy86@naver.com>
2022-12-28 22:24:20 +0900

    while write

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -5,12 +7,33 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("==프로그램 시작==");
 		Scanner sc = new Scanner(System.in);
-		
-		String cmd=sc.nextLine();
-		System.out.println("입력 값: "+cmd);
-		
-		System.out.println("==프로그램 끝==");
-		
+		int no = 1;
+		Map<String,String> lists = new HashMap<>();
+				
+		while(true) {
+			System.out.println("명령어 입력 : ");
+			String cmd=sc.nextLine();
+			
+			if(cmd.equals("article write")) {
+				System.out.println("title 입력 : \r");
+				String title = sc.nextLine();
+				System.out.println("content 입력 : \r");
+				String content = sc.nextLine();
+				String totalString = title + "\n" + content;
+				lists.put(no+"", totalString);
+				System.out.println(no+"번 글이 생성되었습니다.");
+				no++;
+			}else if(cmd.equals("show lists")) {
+				for(String key : lists.keySet()) {
+					System.out.println(lists.get(key));
+				}
+			}else if(cmd.equals("exit")) {
+				break;
+			}else {
+				System.out.println("존재하지 않는 명령어 입니다.");
+			}
+		}
+		System.out.println("==프로그램 끝==");		
 		sc.close();
 	}
 
